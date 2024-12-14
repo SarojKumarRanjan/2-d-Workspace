2a6a85d71604f1c67f74fcc204341e74b9fa98ec
SarojKumarRanjan <sarojranjan555@gmail.com>
2024-12-14 22:53:52 +0530

    fix : type fixes to handle formdata

--- apps/http-layer/src/middlewares/admin.middleware.ts
+++ apps/http-layer/src/middlewares/admin.middleware.ts
@@ -1,23 +1,33 @@
 import { asyncHandler } from "../utils/asyncHandler";
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+
 import { resultFormatter } from "../utils/resultFormatter";
+import jwt from "jsonwebtoken";
 
 
 export const adminMiddleware = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  //console.log("headers",req.headers);
+  
   const header = req.headers.authorization;
   const token = header?.split(" ")[1];
+  //console.log(process.env.JWT_SECRET);
+  
+  //console.log(token)
   if (!token) {
     resultFormatter.throw("Token not found", 400);
     return;
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { userId: string, role: string };
+    //console.log("inside try block")
+    const decoded = jwt.verify(token,process.env.JWT_SECRET!) as { userId: string, role: string };
+    //console.log(decoded)
     if (decoded.role !== "admin") {
+      //console.log("You are not authorized")
       resultFormatter.throw("You are not authorized", 401);
       return;
     }
+    //console.log("You are authorized")
     req.userId = decoded.userId;
     
     next();
--- apps/http-layer/src/types/index.ts
+++ apps/http-layer/src/types/index.ts
@@ -1,68 +1,67 @@
-import z from 'zod';
-
+import z from "zod";
 
 export const SignupSchema = z.object({
-    username: z.string(),
-    password: z.string(),
-    role: z.enum(["user", "admin"]),
-})
- export const SigninSchema = z.object({
-    username:z.string(),
-    password:z.string()
- })
+  username: z.string(),
+  password: z.string(),
+  role: z.enum(["user", "admin"]),
+});
+export const SigninSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
 
+export const UpdateMetaDataSchema = z.object({
+  avatarId: z.string(),
+});
 
- export const UpdateMetaDataSchema = z.object({
-    avatarId:z.string()
- })
+export const CreateSpaceSchema = z.object({
+  name: z.string(),
+  mapId: z.string().optional(),
+  dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
+});
 
- export const CreateSpaceSchema = z.object({
-    name:z.string(),
-    mapId:z.string().optional(),
-    dimensions:z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
- })
+export const AddElementSchema = z.object({
+   spaceId: z.string(), 
+   elementId: z.string(), 
+   x: z.preprocess((val) => parseFloat(val as string), z.number()), 
+   y: z.preprocess((val) => parseFloat(val as string), z.number()), 
+ });
 
- export const AddElementSchema = z.object({
-    spaceId: z.string(),
-    elementId: z.string(),
-    x: z.number(),
-    y: z.number(),
+export const CreateElementSchema = z.object({
+  static: z.preprocess((value) => value === "true", z.boolean()),
+  height: z.preprocess((value) => parseInt(value as string, 10), z.number()), 
+  width: z.preprocess((value) => parseInt(value as string, 10), z.number()), 
+});
 
- })
+export const UpdateElementSchema = z.object({
+  thumbnail: z.string().optional(),
+});
 
- export const CreateElementSchema = z.object({
-    thumbnail: z.string().optional(),
-    width: z.number(),
-    height: z.number(),
-    static: z.boolean(),
- })
+export const CreateAvatarSchema = z.object({
+  thumbnail: z.string().optional(),
+  name: z.string(),
+});
 
- export const UpdateElementSchema = z.object({
-   thumbnail:z.string().optional()
- })
+export const CreateMapSchema = z.object({
+   thumbnail: z.string().optional(), 
+   dimensions: z
+     .string()
+     .regex(/^[0-9]{1,4}x[0-9]{1,4}$/, "Dimensions must be in the format 'NxN'"), 
+   name: z.string(), 
+   defaultElements: z.array(
+     z.object({
+       elementId: z.string(), 
+       x: z.preprocess((val) => parseFloat(val as string), z.number()), 
+       y: z.preprocess((val) => parseFloat(val as string), z.number()), 
+     })
+   ),
+ });
 
- export const CreateAvatarSchema = z.object({
-   thumbnail:z.string().optional(),
-    name:z.string()
- })
-
- export const CreateMapSchema = z.object({
-    thumbnail: z.string().optional(),
-    dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
-    name: z.string(),
-    defaultElements: z.array(z.object({
-        elementId: z.string(),
-        x: z.number(),  
-        y: z.number(),
-    }))
- })
-
-
- declare global {
-   namespace Express {
-     export interface Request {
-       role?: "admin" | "user";
-       userId?: string;
-     }
-   }
+declare global {
+  namespace Express {
+    export interface Request {
+      role?: "admin" | "user";
+      userId?: string;
+    }
+  }
 }
